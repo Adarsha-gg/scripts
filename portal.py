59feaa3882667ecb5f5862c0607a4bd03c6653b7
Adarsha-gg <adarshamishra33@gmail.com>
2023-01-31 18:36:19 +0545

    Script to open all my portals

---
+++ portal.py
@@ -0,0 +1,16 @@
+import webbrowser
+
+webbrowser.open('https://admissions.skidmore.edu/apply/status')
+webbrowser.open('https://connect.trincoll.edu/apply/status')
+webbrowser.open('https://www.admissions.carleton.edu/apply/status')
+webbrowser.open('https://admissions.dickinson.edu/apply/status?tab=home')
+webbrowser.open('https://apply.fandm.edu/account/login?r=https%3a%2f%2fapply.fandm.edu%2fapply%2fstatus%3ftab%3dapp%26%3dundefined')
+webbrowser.open('https://admissions.gettysburg.edu/apply/status')
+webbrowser.open('https://access.wlu.edu/portal/status?cmd=home')
+webbrowser.open('https://admissions.colby.edu/apply/status?tab=admissions')
+webbrowser.open('https://connect.oberlin.edu/apply/status')
+webbrowser.open('https://connect.nyu.edu/portal/undergraduate')
+webbrowser.open('https://lehighuadmportal.force.com/apex/ERx_Forms__Homepage')
+webbrowser.open('https://admission.hamilton.edu/portal/applicantportal?tab=applicantHome')
+webbrowser.open('https://admission.grinnell.edu/apply/status')
+webbrowser.open('https://admissions.bennington.edu/apply/status?tab=apps')
